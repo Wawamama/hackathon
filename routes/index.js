a799e9ab4ed62521dd8e5aa041d2239b8b65ac30
Teddy Fers <teddy.fers@gmail.com>
2021-09-23 11:27:06 +0200

    màj index.js

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,6 @@ const mongoose = require('mongoose');
 var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
 var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
 
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
