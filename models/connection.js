db52c5e66e31fe7ff6c967cfb6d6d24bbe917a5c
Teddy Fers <teddy.fers@gmail.com>
2021-09-23 10:55:01 +0200

    add database

--- models/connection.js
+++ models/connection.js
@@ -1,3 +1,18 @@
-// Somtehting here
+var mongoose = require('mongoose');
 
-// fbefdbqefdnpm sta
+var options = {
+    connectTimeoutMS: 5000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+   };
+
+mongoose.connect('mongodb+srv://Dev:Lacapsule@cluster0.92cqp.mongodb.net/Ticketac?retryWrites=true&w=majority',
+   options,
+   function(err) {
+    if (err) {
+      console.log(`error, failed to connect to the database because --> ${err}`);
+    } else {
+      console.info('*** Database Ticketac connection : Success ***');
+    }
+   }
+);
--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,9 @@ var router = express.Router();
 const mongoose = require('mongoose');
 
 // useNewUrlParser ;)
-var options = {
-  connectTimeoutMS: 5000,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
- };
 
 // --------------------- BDD -----------------------------------------------------
-mongoose.connect('mongodb+srv://XXXXXXXX:*********@XXXXXXXX-0hsfc.mongodb.net/Ticketac?retryWrites=true',
-   options,
-   function(err) {
-    if (err) {
-      console.log(`error, failed to connect to the database because --> ${err}`);
-    } else {
-      console.info('*** Database Ticketac connection : Success ***');
-    }
-   }
-);
+
 
 var journeySchema = mongoose.Schema({
   departure: String,
