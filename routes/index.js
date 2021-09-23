d894c1357cc0ffc9e8cd7e7e89dcfa982af8adc4
Marion Saul <marionsaul.dev@gmail.com>
2021-09-23 11:56:08 +0200

    homepage front

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/homepage', async (req, res, next) => {
+  res.render('home', { title: 'Express' });
+})
+
 module.exports = router;
